124c9686f36e36308f8658465a3cdbb966d347ee
Dartora <dartorandroid@gmail.com>
2023-09-04 10:57:57 -0300

    more cleaning

--- nestJS/src/users/users.service.ts
+++ nestJS/src/users/users.service.ts
@@ -7,20 +7,10 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  /**
-   * Here, we have used data mapper approch for this tutorial that is why we
-   * injecting repository here. Another approch can be Active records.
-   */
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  /**
-   * this is function is used to create User in User Entity.
-   * @param createUserDto this will type of createUserDto in which
-   * we have defined what are the keys we are expecting from body
-   * @returns promise of user
-   */
   createUser(createUserDto: CreateUserDto): Promise<User> {
     const user: User = new User();
     user.name = createUserDto.name;
@@ -36,22 +26,10 @@ export class UsersService {
     return this.userRepository.find();
   }
 
-  /**
-   * this function used to get data of use whose id is passed in parameter
-   * @param id is type of number, which represent the id of user.
-   * @returns promise of user
-   */
   viewUser(id: number): Promise<User> {
     return this.userRepository.findOneBy({ id });
   }
 
-  /**
-   * this function is used to updated specific user whose id is passed in
-   * parameter along with passed updated data
-   * @param id is type of number, which represent the id of user.
-   * @param updateUserDto this is partial type of createUserDto.
-   * @returns promise of udpate user
-   */
   updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
     const user: User = new User();
     user.name = updateUserDto.name;
@@ -63,11 +41,6 @@ export class UsersService {
     return this.userRepository.save(user);
   }
 
-  /**
-   * this function is used to remove or delete user from database.
-   * @param id is the type of number, which represent id of user
-   * @returns nuber of rows deleted or affected
-   */
   removeUser(id: number): Promise<{ affected?: number }> {
     return this.userRepository.delete(id);
   }

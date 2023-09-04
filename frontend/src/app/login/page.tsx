f870002782c14343b3ebce271420851254ec378a
Dartora <dartorandroid@gmail.com>
2023-09-04 13:04:47 -0300

    simple template to login route

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,79 @@
+'use client'
+import { Formik, Field, Form, FormikHelpers } from 'formik';
+
+interface Values {
+  username: string;
+  password: string;
+}
+
+export default function LoginForm() {
+  return (
+    <div className="styles.login_box p-3 w-full max-w-xs m-auto mt-1.5">
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+        }}
+        onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <Field
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              name="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <Field
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="******************"
+            />
+            <p className="text-red-500 text-xs italic">Please choose a password.</p>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </Form>
+      </Formik>
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2023 Dartora TI. All rights reserved.
+      </p>
+    </div>
+  );
+}

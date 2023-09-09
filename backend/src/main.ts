155576efca9a5b7682e4c1ac6cc04eb6c7dd64ae
rds <dartorandroid@gmail.com>
2023-09-09 11:40:46 -0300

    integrated the front end to test more easy the login api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const cors = require('cors'); 
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   await app.listen(3001);
 }
 bootstrap();
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    // Configure environment variables
+    env: {
+      BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:3002',
+    },
+  
+    // Other Next.js configurations
+    // ...
+  }
